d863db6aa2c8429e11ceae5cd661a6e98a907f4a
Artem Kozaev <artemkozaev@gmail.com>
2018-02-07 16:43:13 +0300

    Gem update spree_multi_vendor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
+    return if !Rails.env.staging? && !Rails.env.production?
     Raven.user_context(id: session[:current_user_id])
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
