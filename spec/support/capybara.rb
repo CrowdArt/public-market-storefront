b0c8deb62c81e1faa759db4a50cf5abd52e37209
Artem Kozaev <artemkozaev@gmail.com>
2018-02-19 19:50:59 +0300

    Fix capybara screenshot for headless chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,11 +3,7 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 
-Capybara.register_driver(:chrome) do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
+Capybara.register_driver(:selenium_chrome_headless) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w[headless disable-gpu] }
   )
@@ -15,4 +11,4 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
 
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :selenium_chrome_headless
