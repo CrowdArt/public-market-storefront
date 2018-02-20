70291c31f13ba4ff0a5a9519ed13275f66a808c7
Artem Kozaev <artemkozaev@gmail.com>
2018-02-20 19:51:32 +0300

    Run mailhog on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -86,6 +86,11 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.action_mailer.smtp_settings = {
+    address: 'mailhog',
+    port: 1025
+  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
