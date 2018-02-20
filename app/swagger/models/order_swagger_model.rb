f911ff8463905c2c5066ed2999d5ed9b8f814262
Artem Kozaev <artemkozaev@gmail.com>
2018-02-20 17:56:28 +0300

    Add order fetch response schema to swagger

--- app/swagger/models/order_swagger_model.rb
+++ app/swagger/models/order_swagger_model.rb
@@ -3,12 +3,67 @@ module Swagger
     class OrderSwaggerModel
       include Swagger::Blocks
 
-      swagger_schema :Order do
-        property :id do
+      swagger_schema :Shipping_address do
+        property :firstname do
+          key :type, :string
+        end
+        property :lastname do
+          key :type, :string
+        end
+        property :address1 do
+          key :type, :string
+        end
+        property :address2 do
+          key :type, :string
+        end
+        property :city do
+          key :type, :string
+        end
+        property :zipcode do
           key :type, :integer
         end
-        property :number do
+        property :phone do
           key :type, :string
+        end
+        property :alternative_phone do
+          key :type, :string
+        end
+        property :country do
+          key :type, :string
+        end
+        property :state do
+          key :type, :string
+        end
+      end
+
+      swagger_schema :Line_item do
+        property :sku do
+          key :type, :string
+        end
+        property :quantity do
+          key :type, :integer
+        end
+      end
+
+      swagger_schema :Order do
+        property :shipping_address do
+          key :'$ref', :Shipping_address
+        end
+
+        property :line_items do
+          key :type, :array
+
+          items do
+            key :'$ref', :Line_item
+          end
+        end
+
+        property :order_identifier do
+          key :type, :string
+        end
+
+        property :created_at do
+          key :type, :datetime
         end
       end
 
@@ -21,12 +76,26 @@ module Swagger
           end
         end
 
-        key :example, orders: [
-          {
-            number: 'RS2104212',
-            status: 'shipped'
-          }
-        ]
+        key :example,
+            orders: [
+              {
+                shipping_address: {
+                  firstname: 'Peter',
+                  lastname: 'Evans',
+                  address1: 'Folsom 1519, SF',
+                  address2: 'California, US',
+                  city: 'San Francisco',
+                  zipcode: 94001,
+                  phone: '+19997104099',
+                  alternative_phone: 'null',
+                  country: 'US',
+                  state: 'CA'
+                },
+                order_identifier: 'PM2104212',
+                created_at: '2018-02-16T11:15:00.093Z'
+              }
+            ],
+            count: 1
       end
     end
   end
