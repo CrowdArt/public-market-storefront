02c35095ab604311505d8343d6cfdfa69d1503b4
Artem Kozaev <artemkozaev@gmail.com>
2018-02-21 21:03:27 +0300

    Revert "Enable deface precompile in staging/production env [refs #155122349]"
    This reverts commit 8e3249472f46150c9569756c4a41ab30145c85d2.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -97,6 +97,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.deface.enabled = ENV['DEFACE_ENABLED'] == 'true'
 end
