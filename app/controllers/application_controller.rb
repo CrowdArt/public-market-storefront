c95977186d09da21a9adaee8dd93c82e6f51b724
Artem Kozaev <artemkozaev@gmail.com>
2018-02-22 23:27:18 +0300

    Add basic auth to staging [fixes #155430025]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  prepend_before_action :auth_staging, if: -> { Rails.env.staging? }
   before_action :set_raven_context, if: -> { Rails.env.staging? || Rails.env.production? }
 
   private
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
   def set_raven_context
     Raven.user_context(id: session[:current_user_id])
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def auth_staging
+    authenticate_or_request_with_http_basic do |name, password|
+      name == Settings.basic_auth_name && password == Settings.basic_auth_password
+    end
   end
 end
