ea2541d03d7af055a2641c87816862caf5169558
Artem Kozaev <artemkozaev@gmail.com>
2018-02-27 01:38:26 +0300

    Use dockerfile in ci build [publicmarket/storefront#3]

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,7 +3,6 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  # c.ignore_localhost = true
   c.ignore_hosts '127.0.0.1', '0.0.0.0', 'localhost', 'elasticsearch'
   c.configure_rspec_metadata!
   c.default_cassette_options = {
