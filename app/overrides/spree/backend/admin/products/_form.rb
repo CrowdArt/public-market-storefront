db86d4f1692e36c78810135e32212b7c27d2e998
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-02-26 16:49:30 +0300

    Adds product number to form [fixes #155267932]

---
+++ app/overrides/spree/backend/admin/products/_form.rb
@@ -0,0 +1,11 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/products/_form',
+  name: 'Add number in product form',
+  insert_bottom: 'div[data-hook="admin_product_form_additional_fields"]',
+  text: <<-HTML
+          <%= f.field_container :number, class: ['form-group'] do %>
+            <%= f.label :number, Spree.t(:product_number) %>
+            <%= f.text_field :number, class: 'form-control' %>
+          <% end %>
+        HTML
+)
