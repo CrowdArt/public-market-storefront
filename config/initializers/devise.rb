3a534ae3fc9c4809c0857f456b75215e3d2efa36
Artem Kozaev <artemkozaev@gmail.com>
2018-03-01 17:59:28 +0300

    Use secret_key_base as devise secret key [refs #34]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,4 @@
-Devise.secret_key = '89778def80e850bf38a60f4b56537e14a3aef4bb11c38e5db4a00976e9a22979c8144d89fb8c82fc732fd6ba016e4dfe62be'
+Devise.secret_key = Settings.secret_key_base
 
 Devise.setup do |config|
   config.password_length = 8..50
