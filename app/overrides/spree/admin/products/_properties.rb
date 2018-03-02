d696ff7c6ec49fa8a1b451853c36aa1a47c1f22f
Artem Kozaev <artemkozaev@gmail.com>
2018-03-02 14:46:25 +0300

    Add product weight/dimensions under properties [refs #45]

---
+++ app/overrides/spree/admin/products/_properties.rb
@@ -0,0 +1,15 @@
+Deface::Override.new(
+  virtual_path: 'spree/products/_properties',
+  name: 'Add product weight/dimensions under properties',
+  insert_before: 'erb[silent]:contains("reset_cycle(\'properties\')")',
+  text: <<-HTML
+          <tr class="<%= current_cycle %>">
+            <td><strong>Dimensions</strong></td>
+            <td><%= [@product.width, @product.depth, @product.height].join(' x ') %> inches</td>
+          </tr>
+          <tr class="<%= current_cycle %>">
+            <td><strong>Weight</strong></td>
+            <td><%= @product.weight %> ounces</td>
+          </tr>
+        HTML
+)
--- app/overrides/spree/frontend/products/show.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/products/show',
-  name: 'Delete similar items from product page',
-  remove: '#taxon-crumbs'
-)
