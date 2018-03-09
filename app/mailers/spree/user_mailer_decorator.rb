a9aa20480c126b89b7d5144f47af27781af769f8
Artem Kozaev <artemkozaev@gmail.com>
2018-03-09 16:52:55 +0300

    Move reset_password_instructions email to yml [refs #63]

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -6,6 +6,12 @@ Spree::UserMailer.class_eval do
     mail_template(user, :welcome, root_url: spree.root_url)
   end
 
+  def reset_password_instructions(user, token, _opts = {})
+    edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
+
+    mail_template(user, :reset_password_instructions, edit_password_reset_url: edit_password_reset_url)
+  end
+
   def confirmation_instructions(user, token, _opts = {})
     confirmation_url = spree.spree_user_confirmation_url(confirmation_token: token)
 
