c21b0e9316e0270b4a7ceddd544a684b329dbd61
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-03-12 11:44:52 +0300

    Users receive device/client-friendly emails on mobile/desktop (template optimization) [publicmarket/storefront#61]

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,9 +1,8 @@
 Spree::UserMailer.class_eval do
   def welcome(user_id)
     user = Spree::User.find(user_id)
-    @home_url = spree.root_url
 
-    mail_template(user, :welcome, root_url: spree.root_url)
+    mail_template(user, :welcome, root_url: spree.root_url(host: Spree::Store.current.url))
   end
 
   def reset_password_instructions(user, token, _opts = {})
