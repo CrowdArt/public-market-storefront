7e6a420edd9eb963da3bdd940e0bd90974dfe0f1
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-03-14 12:33:00 +0300

    User can checkout easily & fast without unnecessary fields/work [publicmarket/storefront#77]

--- app/overrides/spree/frontend/checkout/_payment.rb
+++ app/overrides/spree/frontend/checkout/_payment.rb
@@ -4,3 +4,10 @@ Deface::Override.new(
   insert_bottom: 'ul#payment-methods',
   partial: 'spree/checkout/billing_address'
 )
+
+Deface::Override.new(
+  virtual_path: 'spree/checkout/_payment',
+  name: 'Stylize cards radio buttons',
+  insert_after: 'div.card_options label',
+  text: "<br class='visible-xs-block' />"
+)
--- vendor/assets/javascripts/spree/frontend/checkoutPayment.js
+++ vendor/assets/javascripts/spree/frontend/checkoutPayment.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   $('#use_existing_card_yes').click(function() {
     $('#stripe-agreement').hide()
     $('#payment-form-button').addClass('col-md-offset-8 col-sm-offset-6')
+    $('#existing_cards').show()
   })
 
   $('#use_existing_card_no').click(function() {
     $('#stripe-agreement').show()
     $('#payment-form-button').removeClass('col-md-offset-8 col-sm-offset-6')
+    $('#existing_cards').hide()
   })
 })
