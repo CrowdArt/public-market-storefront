793ce53f2ec2a07a58bc24fb08ccae75028da8dc
Artem Kozaev <artemkozaev@gmail.com>
2018-03-14 14:44:32 +0300

    Fixes payment method form submit on click inside buttons container [refs #70]

--- vendor/assets/javascripts/spree/frontend/checkoutPayment.js
+++ vendor/assets/javascripts/spree/frontend/checkoutPayment.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  $('#use_existing_card_yes').click(function() {
-    $('#stripe-agreement').hide()
-    $('#payment-form-button').addClass('col-md-offset-8 col-sm-offset-6')
-    $('#existing_cards').show()
-  })
+  if ($('#checkout_form_payment').length) {
+    $('#use_existing_card_yes').click(function() {
+      $('#stripe-agreement').hide()
+      $('#payment-form-button').addClass('col-md-offset-8 col-sm-offset-6')
+      $('#existing_cards').show()
+    })
 
-  $('#use_existing_card_no').click(function() {
-    $('#stripe-agreement').show()
-    $('#payment-form-button').removeClass('col-md-offset-8 col-sm-offset-6')
-    $('#existing_cards').hide()
-  })
+    $('#use_existing_card_no').click(function() {
+      $('#stripe-agreement').show()
+      $('#payment-form-button').removeClass('col-md-offset-8 col-sm-offset-6')
+      $('#existing_cards').hide()
+    })
+  }
 })
