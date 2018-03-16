b7fea9045e963a51f212aa875bf87743f50b652e
Artem Kozaev <artemkozaev@gmail.com>
2018-03-16 16:39:12 +0300

    Assign variants in db samples to vendor

--- db/samples/variants.rb
+++ db/samples/variants.rb
@@ -1,10 +1,11 @@
 require 'ffaker'
 
 Spree::Product.find_each do |product|
+  vendor = Spree::Vendor.first
   product.variants =
     Array.new(2) do
       sku = FFaker::IdentificationMX.curp
-      Spree::Variant.where(sku: sku, product: product).first_or_initialize do |variant|
+      Spree::Variant.where(sku: sku, product: product, vendor: vendor).first_or_initialize do |variant|
         variant.sku = sku
         variant.price = Random.rand(20)
         variant.option_values = Spree::OptionValue.order('RANDOM()').first(2)
