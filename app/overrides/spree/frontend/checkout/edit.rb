0ed86ef8ebee7c9c294b9cdcd7c65aae7c66590f
Artem Kozaev <artemkozaev@gmail.com>
2018-03-27 19:05:11 +0300

    Remove empty and redundant files

--- app/overrides/spree/frontend/checkout/edit.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/checkout/edit',
-  name: 'Remove email field in checkout flow',
-  remove: 'erb[silent]:contains("if @order.state == \'address\' || !@order.email?")',
-  closing_selector: "erb[silent]:contains('end')"
-)
