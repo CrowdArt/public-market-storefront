2dfd34737b7162735606bd7ce28b893ef2ee6604
Artem Kozaev <artemkozaev@gmail.com>
2018-03-27 14:55:55 +0300

    Apply rubocop fixes

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < Spree::StoreController
   def not_found
-    render :common_error_page, status: 404
+    render :common_error_page, status: :not_found
   end
 
   def internal_server_error
-    render :common_error_page, status: 500
+    render :common_error_page, status: :internal_server_error
   end
 end
