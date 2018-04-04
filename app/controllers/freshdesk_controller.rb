6d79645c4bac859704c2915ff5a9a733807ced8d
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-04-04 10:37:47 +0300

    Escape redirect params to Freshdesk SSO [publicmarket/storefront#102]

--- app/controllers/freshdesk_controller.rb
+++ app/controllers/freshdesk_controller.rb
@@ -5,10 +5,11 @@ class FreshdeskController < ApplicationController
     utctime = time_in_utc
     redirect_url = [
       Settings.freshdesk_url,
-      'login/sso?name=', spree_current_user.full_name,
-      '&email=', spree_current_user.email,
-      '&timestamp=', utctime,
-      '&hash=', gen_hash_from_params_hash(utctime)
+      'login/sso?',
+      { name: spree_current_user.full_name_or_email,
+        email: spree_current_user.email,
+        timestamp: utctime,
+        hash: gen_hash_from_params_hash(utctime) }.to_query
     ].join
     redirect_to(redirect_url)
   end
@@ -16,11 +17,14 @@ class FreshdeskController < ApplicationController
   private
 
   def gen_hash_from_params_hash(utctime)
-    digest = OpenSSL::Digest::Digest.new('MD5')
+    digest = OpenSSL::Digest.new('MD5')
     OpenSSL::HMAC.hexdigest(
       digest,
       Settings.freshdesk_token,
-      [spree_current_user.full_name, Settings.freshdesk_token, spree_current_user.email, utctime].join
+      [spree_current_user.full_name_or_email,
+       Settings.freshdesk_token,
+       spree_current_user.email,
+       utctime].join
     )
   end
 
