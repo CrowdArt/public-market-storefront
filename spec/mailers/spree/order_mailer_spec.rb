fac73f5472b667bbc01efae1dc29a0df9e4fb0fd
Artem Kozaev <artemkozaev@gmail.com>
2018-04-11 18:36:37 +0300

    Add order canceled email spec

--- spec/mailers/spree/order_mailer_spec.rb
+++ spec/mailers/spree/order_mailer_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe Spree::OrderMailer, type: :mailer do
   let(:order) { create(:order_with_line_items) }
 
   before do
+    allow(Spree::Vendor).to receive(:first) { build_stubbed(:vendor) }
     stub_current_store
   end
 
@@ -14,6 +15,29 @@ RSpec.describe Spree::OrderMailer, type: :mailer do
 
     it 'contains correct body' do
       expect(mail.body).to include('Please review and retain the following order information for your records.')
+    end
+
+    it 'container support url' do
+      expect(mail.body).to include('/freshdesk')
+    end
+
+    it 'sends an email' do
+      expect {
+        mail.deliver_now
+      }.to change(ActionMailer::Base.deliveries, :size).by(1)
+    end
+  end
+
+  describe '#cancel_email' do
+    let(:order) { build_stubbed(:order_with_line_items) }
+    let(:mail) { described_class.cancel_email(order) }
+
+    it 'contains correct subject' do
+      expect(mail.subject).to include('could not be filled')
+    end
+
+    it 'contains correct body' do
+      expect(mail.body).to include('vendor is unable to fill this order.')
     end
 
     it 'container support url' do
