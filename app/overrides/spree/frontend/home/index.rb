d450a4b03951a99d16f762faaeaf2490660a3472
Peter Gumeson <gumeson@gmail.com>
2018-04-11 11:36:08 -0600

    Mixpanel track homepage view

---
+++ app/overrides/spree/frontend/home/index.rb
@@ -0,0 +1,13 @@
+Deface::Override.new(
+  virtual_path: 'spree/home/index',
+  name: 'Mixpanel view homepage event',
+  insert_top: '[data-hook=homepage_products]',
+  disabled: !['staging', 'production'].include?(Rails.env),
+  text: <<-HTML
+<script type="text/javascript">
+  mixpanel.track(
+    "homepage view"
+  );
+</script>
+HTML
+)
