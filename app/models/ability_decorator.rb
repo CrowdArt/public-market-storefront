d75b221149c9b19333c8e8d2ba8d15deb1703ace
Artem Kozaev <artemkozaev@gmail.com>
2018-04-11 17:35:47 +0300

    Add ability specs

--- app/models/ability_decorator.rb
+++ app/models/ability_decorator.rb
@@ -2,17 +2,19 @@ class AbilityDecorator
   include CanCan::Ability
 
   def initialize(user)
+    persisted_abilities(user) if user.persisted?
+  end
+
+  private
+
+  def persisted_abilities(user)
     can :rate, Spree::Order, user_id: user.id
 
+    can :create, Spree::Address
     can :manage, Spree::Address, user_id: user.id
-    can :create, Spree::Address do
-      user.id.present?
-    end
 
+    can :create, Spree::CreditCard
     can :manage, Spree::CreditCard, user_id: user.id
-    can :create, Spree::CreditCard do
-      user.id.present?
-    end
   end
 end
 
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,61 @@
+require 'cancan/matchers'
+
+RSpec.describe AbilityDecorator, type: :ability do
+  subject(:ability) { Spree::Ability.new(user) }
+
+  let(:user) { nil }
+
+  context 'when is guest' do
+    it { is_expected.not_to be_able_to(:rate, Spree::Order) }
+    it { is_expected.not_to be_able_to(:create, Spree::Address) }
+    it { is_expected.not_to be_able_to(:create, Spree::CreditCard) }
+  end
+
+  context 'when is registered user' do
+    let(:user) { build_stubbed(:user) }
+
+    describe 'rate' do
+      let(:order) { build_stubbed(:order) }
+
+      context 'with own order' do
+        let(:order) { build_stubbed(:order, user: user) }
+
+        it { is_expected.to be_able_to(:rate, order) }
+      end
+
+      context "with other's order" do
+        let(:order) { build_stubbed(:order) }
+
+        it { is_expected.not_to be_able_to(:rate, order) }
+      end
+    end
+
+    shared_examples 'manage abilities' do |model|
+      describe 'edit' do
+        context 'with own' do
+          let(:object) { build_stubbed(model, user: user) }
+
+          it { is_expected.to be_able_to(:manage, object) }
+        end
+
+        context "with other's" do
+          let(:object) { build_stubbed(model) }
+
+          it { is_expected.not_to be_able_to(:manage, object) }
+        end
+      end
+    end
+
+    describe 'address' do
+      it { is_expected.to be_able_to(:create, Spree::Address) }
+
+      include_examples 'manage abilities', :address
+    end
+
+    describe 'credit card' do
+      it { is_expected.to be_able_to(:create, Spree::CreditCard) }
+
+      include_examples 'manage abilities', :credit_card
+    end
+  end
+end
