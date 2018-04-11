9e3ddde9936c41f5194b7f03500bbbb64cf81bcf
Peter Gumeson <gumeson@gmail.com>
2018-04-11 11:38:50 -0600

    Mixpanel track product view

---
+++ app/overrides/spree/frontend/products/show.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(
+  virtual_path: 'spree/products/show',
+  name: 'Mixpanel view product event',
+  insert_top: '[data-hook=product_show]',
+  disabled: !['staging', 'production'].include?(Rails.env),
+  text: <<-HTML
+<script type="text/javascript">
+  mixpanel.track("product view", {
+    "id": "<%= @product.id %>",
+    "name": "<%= @product.name %>"
+  });
+</script>
+HTML
+)
