36c59cfb88b78c8f7e2655fbe8082bd7c3be4761
Peter Gumeson <gumeson@gmail.com>
2018-04-12 20:38:10 -0600

    Mixpanel track email confirmation events

--- app/controllers/devise/confirmations_controller_decorator.rb
+++ app/controllers/devise/confirmations_controller_decorator.rb
@@ -7,9 +7,32 @@ Devise::ConfirmationsController.class_eval do
 
     if resource.errors.empty?
       set_flash_message!(:notice, notification_msg)
+      flash[:mixpanel_actions] = [
+        %Q(mixpanel.track("email confirmed", {
+          "user_id": "#{resource.id}",
+          "email": "#{resource.try(:email)}"
+        }))
+      ].to_json
       respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
     else
       respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
+    end
+  end
+
+  def create # rubocop:disable Metrics/AbcSize
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      flash[:mixpanel_actions] = [
+        %Q(mixpanel.track("email confirmation sent", {
+          "user_id": "#{resource.id}",
+          "email": "#{resource.try(:email)}"
+        }))
+      ].to_json
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
     end
   end
 end
