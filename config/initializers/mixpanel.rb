07ea10348a6a34ef6a3dd6252b8f1cf0e8d8cc3a
Peter Gumeson <gumeson@gmail.com>
2018-04-13 18:19:00 -0600

    Make mixpanel tracker noop when not prod or stage env

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,9 +1,18 @@
 require 'mixpanel-ruby'
 
-::Tracker = Mixpanel::Tracker.new(Settings.mixpanel_api_key)
+module Tracker
+  class << self
+    # Only log in production and staging
+    def method_missing(name, *args, &block)
+      if ['staging', 'production'].include?(Rails.env)
+        tracker.public_send(name, *args) if tracker.respond_to?(name)
+      end
+    end
 
-# if Rails.env.development?
-#   Mixpanel.config_http do |http|
-#     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-#   end
-# end
+    private
+
+    def tracker
+      @@tracker ||= Mixpanel::Tracker.new(Settings.mixpanel_api_key)
+    end
+  end
+end
