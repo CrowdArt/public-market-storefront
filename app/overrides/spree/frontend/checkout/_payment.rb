476d9ae85aa5691084696f8607bd38042f81fcdf
Artem Kozaev <artemkozaev@gmail.com>
2018-04-20 14:02:48 +0300

    Move payment partial override to view

--- app/overrides/spree/frontend/checkout/_payment.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/checkout/_payment',
-  name: 'Stylize cards radio buttons',
-  insert_after: 'div.card_options label',
-  text: "<br class='visible-xs-block' />"
-)
