37091aa92001c1856a3745275d0a69e17c938273
Artem Kozaev <artemkozaev@gmail.com>
2018-05-02 19:35:46 +0300

    Navbar UI [refs #157031013]

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -32,5 +32,20 @@ module Spree
     def compared_user_addresses(address)
       spree_current_user.addresses.map { |a| [a, address.same_as?(a)] }
     end
+
+    def link_to_cart(text = nil)
+      text = text ? h(text) : Spree.t('cart')
+      css_class = nil
+
+      if simple_current_order.nil? || simple_current_order.item_count.zero?
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span>"
+        css_class = 'empty'
+      else
+        text = "<span class='glyphicon glyphicon-shopping-cart'></span><span class='badge'>#{simple_current_order.item_count}</span>"
+        css_class = 'full'
+      end
+
+      link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}" # rubocop:disable Rails/OutputSafety
+    end
   end
 end
