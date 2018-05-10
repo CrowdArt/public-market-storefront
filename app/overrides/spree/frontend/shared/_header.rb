9c660b3888d6cdcb9f323909f4c61597791623a2
Artem Kozaev <artemkozaev@gmail.com>
2018-05-11 01:34:24 +0300

    Move redundant logo override [refs #157477995]

--- app/overrides/spree/frontend/shared/_header.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/shared/_header',
-  name: 'Change bootstrap grid on logo',
-  set_attributes: 'figure#logo',
-  attributes: { class: 'col-sm-2 col-xs-5' }
-)
