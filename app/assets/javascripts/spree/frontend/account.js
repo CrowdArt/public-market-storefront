2291ece330227fa6a343904130cc950953875a42
Artem Kozaev <artemkozaev@gmail.com>
2018-05-10 20:28:08 +0300

    Implement mobile menu [refs #157031059]

--- app/assets/javascripts/spree/frontend/account.js
+++ app/assets/javascripts/spree/frontend/account.js
@@ -2,8 +2,8 @@ Spree.fetch_account = function() {
   return $.ajax({
     url: Spree.pathFor("account_link"),
     success: function(data) {
-      $('#spree-header .link-to-account').remove();
-      return $(data).insertBefore("li#link-to-cart");
+      $('#spree-header .nav-buttons .link-to-account').remove();
+      return $(data).insertBefore(".nav-buttons li#link-to-cart");
     }
   });
 };
