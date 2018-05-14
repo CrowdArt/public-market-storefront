34cbfc4348f8dd4ab0868c3e7ee76cf19b240d7f
Artem Kozaev <artemkozaev@gmail.com>
2018-05-14 13:20:59 +0300

    Remove redundant account fetch script override

--- app/assets/javascripts/spree/frontend/account.js
+++
@@ -1,9 +0,0 @@
-Spree.fetch_account = function() {
-  return $.ajax({
-    url: Spree.pathFor("account_link"),
-    success: function(data) {
-      $('#spree-header .navbar-top-right .link-to-account').remove();
-      return $(data).insertBefore(".navbar-top-right li#link-to-cart");
-    }
-  });
-};
