0087c82631814565ed49767980cedbd7aa74e059
Artem Kozaev <artemkozaev@gmail.com>
2018-05-25 17:45:59 +0300

    Fixes current user set in sentry tracker [fixes #157829243]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
-    Raven.user_context(id: session[:current_user_id])
+    Raven.user_context(user_id: spree_current_user&.id)
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 
