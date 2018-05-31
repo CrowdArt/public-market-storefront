f6b7090cfefa9018290dd53669ef01ad95fc2ef2
Artem Kozaev <artemkozaev@gmail.com>
2018-05-31 14:20:55 +0300

    Fixes deprecation warning in apidocs controller spec

--- spec/controllers/api_docs_controller_spec.rb
+++ spec/controllers/api_docs_controller_spec.rb
@@ -2,13 +2,13 @@ RSpec.describe ApiDocsController, type: :controller do
   describe '#index' do
     subject { get :index }
 
-    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to be_successful }
   end
 
   describe '#schema' do
     subject(:get_schema) { get :schema }
 
-    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to be_successful }
 
     it 'returns swagger json' do
       expect(get_schema.parsed_body).to include('swagger')
