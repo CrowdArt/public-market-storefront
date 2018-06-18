6fbae59f089138c5d1510380cd2333eff971e07a
Artem Kozaev <artemkozaev@gmail.com>
2018-06-18 19:45:18 +0300

    Fixes applicable taxon filters in specs

--- lib/spree/search/taxon_filters.rb
+++ lib/spree/search/taxon_filters.rb
@@ -2,7 +2,8 @@ module Spree
   module Search
     module TaxonFilters
       def self.applicable_filters(aggregations, taxon)
-        taxon.taxonomy.variation_module::Filters.applicable_filters(aggregations)
+        return [] if (variation_module = taxon.taxonomy.variation_module).blank?
+        variation_module::Filters.applicable_filters(aggregations)
       end
     end
   end
