8db3a40d7d453c216902f872fbc19e52e1fc0987
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-07-18 13:09:17 +0300

    Use symbols as FTP keys

--- app/workers/process_order_updates.rb
+++ app/workers/process_order_updates.rb
@@ -6,6 +6,6 @@ class ProcessOrderUpdates
   def perform(vendor_name, ftp_key)
     vendor = Spree::Vendor.find_by(name: vendor_name)
     puts "Process #{vendor_name} order updates from #{ftp_key} FTP"
-    puts Spree::Orders::Ftp::ProcessOrderUpdatesAction.call(vendor, ftp_key) if vendor.present?
+    puts Spree::Orders::Ftp::ProcessOrderUpdatesAction.call(vendor, ftp_key.to_sym) if vendor.present?
   end
 end
--- app/workers/upload_order_report.rb
+++ app/workers/upload_order_report.rb
@@ -6,6 +6,6 @@ class UploadOrderReport
   def perform(vendor_name, ftp_key)
     vendor = Spree::Vendor.find_by(name: vendor_name)
     puts "Upload #{vendor_name} order report to #{ftp_key} FTP"
-    puts Spree::Orders::Ftp::UploadOrderReportAction.call(vendor, ftp_key) if vendor.present?
+    puts Spree::Orders::Ftp::UploadOrderReportAction.call(vendor, ftp_key.to_sym) if vendor.present?
   end
 end
