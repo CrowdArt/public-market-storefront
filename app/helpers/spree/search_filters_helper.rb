1ac178fb5b4860ba3e1bd5624b7cf66a44ba5b23
Artem Kozaev <artemkozaev@gmail.com>
2018-07-24 21:06:42 +0300

    Fixes search filters cache [fixes #158649674]

--- app/helpers/spree/search_filters_helper.rb
+++ app/helpers/spree/search_filters_helper.rb
@@ -1,9 +1,7 @@
 module Spree
   module SearchFiltersHelper
     def search_filters
-      Rails.cache.fetch([:v3, :search_filters, @taxon]) do
-        Spree::Search::SearchkickFilters.applicable_filters(taxon: @taxon)
-      end
+      Spree::Search::SearchkickFilters.applicable_filters(taxon: @taxon)
     end
 
     def search_sorting_options
