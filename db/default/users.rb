704ba0f9f09f2beb22fd0326d3ac92bef593f6d0
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-07-30 12:09:14 +0300

    Change seed files [refs #159312603]

--- db/default/users.rb
+++ db/default/users.rb
@@ -1,4 +1,12 @@
 Spree::User.create!(
+  email: Rails.application.credentials.admin_name,
+  login: 'PM Admin',
+  password: Rails.application.credentials.admin_password,
+  spree_roles: Spree::Role.where(name: 'admin'),
+  confirmed_at: Time.zone.now
+)
+
+Spree::User.create!(
   email: Rails.application.credentials.test_seller_user_name,
   login: Rails.application.credentials.test_seller_user_name,
   password: Rails.application.credentials.test_seller_user_password,
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Spree::User.skip_callback(:create, :after, :send_welcome_email_with_delay)
-
-Spree::Core::Engine.load_seed if defined?(Spree::Core)
-Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 default_path = File.join(File.dirname(__FILE__), 'default')
 
