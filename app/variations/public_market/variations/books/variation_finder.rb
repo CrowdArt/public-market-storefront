8407ede385d34ff684ac1ce92c9ebac8ed698e88
Artem Kozaev <artemkozaev@gmail.com>
2018-07-30 13:09:00 +0300

    Remove root variation from variation boxes [refs #159164943]

--- app/variations/public_market/variations/books/variation_finder.rb
+++ app/variations/public_market/variations/books/variation_finder.rb
@@ -1,16 +1,7 @@
 module PublicMarket
   module Variations
     module Books
-      class VariationFinder < BaseVariationFinder
-        def self.variation_name(format, _variation)
-          return if format.blank?
-
-          child = I18n.t("variations.titleized-format.#{format}", default: format.titleize)
-          parent = Properties.find_matching_format([format], Properties.filterable_book_formats)
-
-          [parent.map(&:titleize), child].flatten.uniq.join(': ')
-        end
-      end
+      class VariationFinder < BaseVariationFinder; end
     end
   end
 end
