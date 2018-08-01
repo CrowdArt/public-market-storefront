b9fdd21ee5fc2d2a850ddc52896437492cf1fdc5
Artem Kozaev <artemkozaev@gmail.com>
2018-08-01 23:16:58 +0300

    Fixes long variations [fixes #159164943]

--- app/variations/public_market/variations/books/variation_finder.rb
+++ app/variations/public_market/variations/books/variation_finder.rb
@@ -2,18 +2,18 @@ module PublicMarket
   module Variations
     module Books
       class VariationFinder < BaseVariationFinder
-        def self.variation_name(variation, product)
-          return if variation.blank?
+        def self.variation_name(root_variation, product)
+          return if root_variation.blank?
 
           product_variation = Spree::Product.find_by(id: product[:_id]).variation
 
           child = I18n.t("variations.titleized-format.#{product_variation}", default: product_variation.titleize)
 
-          if variation == 'other'
-            [variation.titleize, child].flatten.uniq.join(': ')
-          else
-            child
-          end
+          titleized_variation = root_variation.titleize
+          # remove root format from unique PM format
+          child = child.remove(titleized_variation).strip
+
+          [titleized_variation, child].reject(&:blank?).uniq.join(': ')
         end
       end
     end
