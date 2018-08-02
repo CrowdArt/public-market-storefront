94c39af1623ada832e0857d2761a8129bfcff6bc
Artem Kozaev <artemkozaev@gmail.com>
2018-08-02 18:45:29 +0300

    Fixes check for assest existence [fixes #159499356]

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,5 +1,10 @@
 module AssetsHelper
   def asset_exists?(path)
-    Rails.application.assets_manifest.find(path).any?
+    # https://github.com/rails/sprockets-rails/issues/298#issuecomment-168927471
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include?(path)
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
   end
 end
