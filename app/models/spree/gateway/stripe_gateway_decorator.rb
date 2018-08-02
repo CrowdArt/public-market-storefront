4ae616038bc1834dd62ac37f6c5441799ffd20e6
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-08-02 11:37:12 +0300

    Buyers/Sellers see Public Market & order id in their billing statements [fixes #159374616]

--- app/models/spree/gateway/stripe_gateway_decorator.rb
+++ app/models/spree/gateway/stripe_gateway_decorator.rb
@@ -16,6 +16,22 @@ Spree::Gateway::StripeGateway.class_eval do
   def reverse_transfer(money, transfer_id)
     provider.reverse_transfer(money, transfer_id: transfer_id)
   end
+
+  def options_for_purchase_or_auth(money, creditcard, gateway_options)
+    options = {}
+    options[:description] = Spree.t('payment_description', order_id: gateway_options[:order_id])
+    options[:currency] = gateway_options[:currency]
+
+    if (customer = creditcard.gateway_customer_profile_id)
+      options[:customer] = customer
+    end
+
+    if (token_or_card_id = creditcard.gateway_payment_profile_id)
+      creditcard = token_or_card_id
+    end
+
+    [money, creditcard, options]
+  end
 end
 
 ActiveMerchant::Billing::StripeGateway.class_eval do
