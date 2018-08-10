e8927f3834fc976a05c2083a19b103e69d250b2f
Artem Kozaev <artemkozaev@gmail.com>
2018-08-10 14:03:06 +0300

    Set max length of card/address nickname [fixes #159536918]

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,31 +1,39 @@
-Spree::Address.class_eval do
-  acts_as_paranoid
+module Spree
+  module AddressDecorator
+    EXCLUDED_KEYS_FOR_COMPARISION = %w[id user_id updated_at created_at].freeze
+    MAX_NICKNAME_LENGTH = 25
 
-  belongs_to :user, class_name: Spree.user_class.to_s, optional: true, inverse_of: :addresses
+    def self.prepended(base)
+      base.acts_as_paranoid
 
-  phony_normalize :phone, default_country_code: 'US'
-  validates :phone, phony_plausible: true
+      base.belongs_to :user, class_name: Spree.user_class.to_s, optional: true, inverse_of: :addresses
 
-  after_save :ensure_one_default
+      base.phony_normalize :phone, default_country_code: 'US'
+      base.validates :phone, phony_plausible: true
+      base.validates :title, length: { maximum: MAX_NICKNAME_LENGTH }, allow_blank: true
 
-  EXCLUDED_KEYS_FOR_COMPARISION = %w[id user_id updated_at created_at].freeze
+      base.after_save :ensure_one_default
+    end
 
-  def same_as?(other)
-    return false if other.nil?
-    attributes.except(*EXCLUDED_KEYS_FOR_COMPARISION) == other.attributes.except(*EXCLUDED_KEYS_FOR_COMPARISION)
+    def same_as?(other)
+      return false if other.nil?
+      attributes.except(*EXCLUDED_KEYS_FOR_COMPARISION) == other.attributes.except(*EXCLUDED_KEYS_FOR_COMPARISION)
+    end
+
+    def full_address
+      [address1, city, "#{state.abbr} #{zipcode}", country].join(', ')
+    end
+
+    private
+
+    def ensure_one_default
+      return if user_id.blank? || !default
+
+      Spree::Address.where(default: true, user_id: user_id)
+                    .where.not(id: id)
+                    .update_all(default: false) # rubocop:disable Rails/SkipsModelValidations
+    end
   end
 
-  def full_address
-    [address1, city, "#{state.abbr} #{zipcode}", country].join(', ')
-  end
-
-  private
-
-  def ensure_one_default
-    return if user_id.blank? || !default
-
-    Spree::Address.where(default: true, user_id: user_id)
-                  .where.not(id: id)
-                  .update_all(default: false) # rubocop:disable Rails/SkipsModelValidations
-  end
+  Address.prepend(AddressDecorator)
 end
--- app/models/spree/credit_card_decorator.rb
+++ app/models/spree/credit_card_decorator.rb
@@ -1,85 +1,93 @@
-Spree::CreditCard.class_eval do
-  include Spree::StripeCustomer
-  extend Enumerize
+module Spree
+  module CreditCardDecorator
+    MAX_NICKNAME_LENGTH = 25
 
-  extend FriendlyId
-  friendly_id :slug_candidates, use: %i[slugged scoped], scope: :user
+    def self.prepended(base)
+      base.include(Spree::StripeCustomer)
+      base.extend(Enumerize)
+      base.extend(FriendlyId)
 
-  acts_as_paranoid
+      base.friendly_id :slug_candidates, use: %i[slugged scoped], scope: :user
 
-  belongs_to :address, class_name: 'Spree::Address', required: true, dependent: :destroy
-  accepts_nested_attributes_for :address
+      base.acts_as_paranoid
 
-  validates :month, :year, numericality: { only_integer: true }
-  validates :name, presence: true
+      base.belongs_to :address, class_name: 'Spree::Address', required: true, dependent: :destroy
+      base.accepts_nested_attributes_for :address
 
-  before_validation :clone_shipping_address, if: :use_shipping?
+      base.validates :month, :year, numericality: { only_integer: true }
+      base.validates :name, length: { maximum: MAX_NICKNAME_LENGTH }, allow_blank: true
 
-  # use enumerize instead of simple enum, because using enum lead to error in class_eval https://github.com/spree/spree/issues/5786
-  enumerize :funding, in: { credit: 0, debit: 1, unknown: 2, prepaid: 3 }, default: :credit, predicates: { prefix: true }
+      base.before_validation :clone_shipping_address, if: :use_shipping?
 
-  delegate :provider, to: :payment_method
+      # use enumerize instead of simple enum, because using enum lead to error in class_eval https://github.com/spree/spree/issues/5786
+      base.enumerize :funding, in: { credit: 0, debit: 1, unknown: 2, prepaid: 3 }, default: :credit, predicates: { prefix: true }
 
-  attr_accessor :use_shipping
+      base.delegate :provider, to: :payment_method
 
-  def expiry=(expiry) # rubocop:disable Metrics/AbcSize
-    self[:month], self[:year] =
-      if expiry.match?(%r{\d{2}\s?\/\s?\d{2,4}}) # will match mm/yy and mm / yyyy
-        expiry.delete(' ').split('/')
-      elsif (match = expiry.match(/(\d{2})(\d{2,4})/)) # will match mmyy and mmyyyy
-        [match[1], match[2]]
-      end
-
-    if self[:year]
-      self[:year] = "20#{self[:year]}" if (self[:year] / 100).zero?
-      self[:year] = self[:year].to_i
+      base.attr_accessor :use_shipping
     end
 
-    self[:month] = self[:month].to_i if self[:month]
-  end
+    def expiry=(expiry) # rubocop:disable Metrics/AbcSize
+      self[:month], self[:year] =
+        if expiry.match?(%r{\d{2}\s?\/\s?\d{2,4}}) # will match mm/yy and mm / yyyy
+          expiry.delete(' ').split('/')
+        elsif (match = expiry.match(/(\d{2})(\d{2,4})/)) # will match mmyy and mmyyyy
+          [match[1], match[2]]
+        end
 
-  def display_number(show_prefix: false)
-    return '' unless last_digits
-    prefix =
-      case show_prefix
-      when false
-        nil
-      when :short
-        'x'
-      else
-        'XXXX-XXXX-XXXX'
+      if self[:year]
+        self[:year] = "20#{self[:year]}" if (self[:year] / 100).zero?
+        self[:year] = self[:year].to_i
       end
 
-    "#{prefix}-#{last_digits}"
-  end
-
-  def name_with_initial
-    first_name, last_name = name.split(' ')
-    "#{first_name} #{last_name&.first}."
-  end
-
-  def use_shipping?
-    use_shipping.present?
-  end
-
-  private
-
-  def clone_shipping_address
-    if (user_address = user.addresses.find_by(id: use_shipping)).present?
-      if address.blank?
-        self.address = user_address.clone
-      else
-        address.attributes = user_address.attributes.except('id', 'updated_at', 'created_at')
-      end
+      self[:month] = self[:month].to_i if self[:month]
     end
 
-    address.user_id = nil
+    def display_number(show_prefix: false)
+      return '' unless last_digits
+      prefix =
+        case show_prefix
+        when false
+          nil
+        when :short
+          'x'
+        else
+          'XXXX-XXXX-XXXX'
+        end
+
+      "#{prefix}-#{last_digits}"
+    end
+
+    def name_with_initial
+      first_name, last_name = name.split(' ')
+      "#{first_name} #{last_name&.first}."
+    end
+
+    def use_shipping?
+      use_shipping.present?
+    end
+
+    private
+
+    def clone_shipping_address
+      if (user_address = user.addresses.find_by(id: use_shipping)).present?
+        if address.blank?
+          self.address = user_address.clone
+        else
+          address.attributes = user_address.attributes.except('id', 'updated_at', 'created_at')
+        end
+      end
+
+      address.user_id = nil
+    end
+
+    def slug_candidates
+      [
+        :card_name,
+        [:card_name, -> { SecureRandom.uuid.first(5) }]
+      ]
+    end
   end
 
-  def slug_candidates
-    [
-      :card_name,
-      [:card_name, -> { SecureRandom.uuid.first(5) }]
-    ]
-  end
+  CreditCard.prepend(CreditCardDecorator)
 end
