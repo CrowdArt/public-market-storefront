69cbb1fc409ec832e02ffbc1d2006aef5346ec4b
Artem Kozaev <artemkozaev@gmail.com>
2018-08-13 17:47:12 +0300

    Show bold credit card name/type [fixes #159536918]

--- app/helpers/spree/payment_helper.rb
+++ app/helpers/spree/payment_helper.rb
@@ -3,5 +3,9 @@ module Spree
     def payment_method_dropdown_item(card)
       [card.name_with_initial, card.cc_type&.humanize, card.display_number(show_prefix: :short)].join(' ')
     end
+
+    def card_name_type(card)
+      [card.card_name.presence || card.cc_type&.humanize&.upcase, card.display_number].join(' ')
+    end
   end
 end
