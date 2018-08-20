d8e4b4bad14665362c606c19905bc8bdcb226acd
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-08-20 17:00:22 +0300

    Disable error logging and debugger

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -103,8 +103,8 @@ Rails.application.configure do
 
   config.deface.enabled = ENV['DEFACE_ENABLED'] == 'true'
 
-  config.google_cloud.use_error_reporting = true
-  config.google_cloud.use_debugger = true
+  # config.google_cloud.use_error_reporting = true
+  # config.google_cloud.use_debugger = true
   config.google_cloud.use_logging = true
   config.google_cloud.use_trace = true
 end
