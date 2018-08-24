772fc6b95d5da9bc25f7f4952771209df92fa6c6
Artem Kozaev <artemkozaev@gmail.com>
2018-08-24 16:46:57 +0300

    Remove login assignment in user seeds

--- db/default/10_users.rb
+++ db/default/10_users.rb
@@ -1,6 +1,5 @@
 Spree::User.create!(
   email: Rails.application.credentials.admin_name,
-  login: 'PM Admin',
   password: Rails.application.credentials.admin_password,
   spree_roles: Spree::Role.where(name: 'admin'),
   confirmed_at: Time.zone.now
@@ -8,7 +7,6 @@ Spree::User.create!(
 
 Spree::User.create!(
   email: Rails.application.credentials.test_seller_user_name,
-  login: Rails.application.credentials.test_seller_user_name,
   password: Rails.application.credentials.test_seller_user_password,
   spree_api_key: Rails.application.credentials.test_seller_user_api_key,
   spree_roles: Spree::Role.where(name: 'user'),
