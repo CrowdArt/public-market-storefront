3c7e795ef032d25b854f1eae672558680a711e24
Artem Kozaev <artemkozaev@gmail.com>
2018-09-03 19:21:48 +0300

    Fixes vinyl speed case [fixes #160087010]

--- app/variations/public_market/variations/music/variation_finder.rb
+++ app/variations/public_market/variations/music/variation_finder.rb
@@ -6,12 +6,14 @@ module PublicMarket
           def variation_name(product, variation = product.variation)
             return if variation.blank?
 
+            name = I18n.t("variations.titleized-format.#{variation}", default: variation.titleize)
+
             if variation == 'vinyl'
               vinyl_speed = product.respond_to?(:property) ? product.property(:vinyl_speed) : product[:vinyl_speed]
-              variation += " (#{vinyl_speed.upcase})" if vinyl_speed
+              name += " (#{vinyl_speed.upcase})" if vinyl_speed
             end
 
-            I18n.t("variations.titleized-format.#{variation}", default: variation.titleize)
+            name
           end
         end
       end
