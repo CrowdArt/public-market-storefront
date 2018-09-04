31ad0f83ddfc47f75dbee3687620d23f3a59dd70
Artem Kozaev <artemkozaev@gmail.com>
2018-09-04 14:42:04 +0300

    Implement buy box UI fixes [fixes #160151934]

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -30,7 +30,8 @@ module Spree
 
     def show
       @previous_variation = Spree::Product.find_by(slug: params[:variation])
-      @buy_box_variant = @product.variants.find_best_price_in_option(in_stock: false).first
+      # use out of stock variant if none in stock
+      @buy_box_variant = @product.variants.find_best_price_in_option.first || @product.variants.find_best_price_in_option(in_stock: false).first
       redirect_if_legacy_path
     end
 
