9596dda0c4eedd0f33e864a49207898809fcdf31
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-09-05 16:36:49 +0300

    Use vendor queue for each uploads [fixes #160262454]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@ require 'sidekiq'
 require 'sidekiq/logging'
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 12, db: 14, host: ENV['REDIS_HOST'] }
+  config.redis = { size: 27, db: 14, host: ENV['REDIS_HOST'] }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 12, db: 14, host: ENV['REDIS_HOST'] }
+  config.redis = { size: 27, db: 14, host: ENV['REDIS_HOST'] }
 end
 
 module Sidekiq
