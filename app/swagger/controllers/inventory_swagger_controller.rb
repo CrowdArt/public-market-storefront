b9a1706df2917c70af6b40378dde55b5fcebdb8a
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-09-07 18:59:45 +0300

    Change endpoints descriptions [refs #159977766]

--- app/swagger/controllers/inventory_swagger_controller.rb
+++ app/swagger/controllers/inventory_swagger_controller.rb
@@ -44,7 +44,7 @@ module Swagger
 
       swagger_path '/inventory/{content_format}/books' do
         operation :post do
-          key :summary, 'Upload inventory'
+          key :summary, 'Upload books inventory'
           key :description, 'Uploads inventory within given file with inventory items'
 
           key :tags, [:inventory]
@@ -88,55 +88,9 @@ module Swagger
         end
       end
 
-      swagger_path '/inventory/{content_format}/generic' do
-        operation :post do
-          key :summary, 'Upload inventory'
-          key :description, 'Uploads inventory within given file with inventory items'
-
-          key :tags, [:inventory]
-
-          key :consumes, ['application/json', 'text/csv']
-
-          security do
-            key :pm_token, []
-          end
-
-          parameter do
-            key :name, :content_format
-            key :description, 'Upload format (json or csv)'
-            key :in, :path
-            key :required, true
-            key :type, :string
-            key :enum, %i[json csv]
-            key :example, :json
-          end
-
-          parameter do
-            key :name, :body
-            key :description, 'Inventory items'
-            key :in, :body
-            key :required, true
-
-            schema do
-              property :items do
-                key :type, :array
-                items do
-                  key :'$ref', :GenericInventoryInput
-                end
-              end
-            end
-          end
-
-          extend SwaggerResponses::AuthenticationError
-          response 200 do
-            key :description, 'Returns async job id'
-          end
-        end
-      end
-
       swagger_path '/inventory/{content_format}/music' do
         operation :post do
-          key :summary, 'Upload inventory'
+          key :summary, 'Upload music inventory'
           key :description, 'Uploads inventory within given file with inventory items'
 
           key :tags, [:inventory]
@@ -168,6 +122,52 @@ module Swagger
                 key :type, :array
                 items do
                   key :'$ref', :MusicInventoryInput
+                end
+              end
+            end
+          end
+
+          extend SwaggerResponses::AuthenticationError
+          response 200 do
+            key :description, 'Returns async job id'
+          end
+        end
+      end
+
+      swagger_path '/inventory/{content_format}' do
+        operation :post do
+          key :summary, 'Upload inventory with any product type'
+          key :description, 'Uploads inventory within given file with inventory items'
+
+          key :tags, [:inventory]
+
+          key :consumes, ['application/json', 'text/csv']
+
+          security do
+            key :pm_token, []
+          end
+
+          parameter do
+            key :name, :content_format
+            key :description, 'Upload format (json or csv)'
+            key :in, :path
+            key :required, true
+            key :type, :string
+            key :enum, %i[json csv]
+            key :example, :json
+          end
+
+          parameter do
+            key :name, :body
+            key :description, 'Inventory items'
+            key :in, :body
+            key :required, true
+
+            schema do
+              property :items do
+                key :type, :array
+                items do
+                  key :'$ref', :GenericInventoryInput
                 end
               end
             end
