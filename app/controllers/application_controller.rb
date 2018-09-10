b4d76086db1d05862549fb60f117be38e3f87950
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-09-10 21:04:44 +0300

    Remove waitlist redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_raven_context, if: -> { Rails.env.staging? || Rails.env.production? }
-  before_action :authenticate_user!, unless: :allow_unauthenticated_view?
+  # before_action :authenticate_user!, unless: :allow_unauthenticated_view?
 
   protected
 
